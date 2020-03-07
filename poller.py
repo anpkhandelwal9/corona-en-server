a8eab1f510a1781d00837e7ff54bf7c8504683fe
Anupam Khandelwal <anupamkhandelwal1@gmail.com>
2020-03-07 10:45:36 +0900

    Make webserver methdods generic, add extra threads

--- poller.py
+++ poller.py
@@ -6,19 +6,24 @@ import json
 from time import sleep
 import pytz
 from functools import cmp_to_key
-from copy import deepcopy
+from urllib.parse import urlparse
 
 class NaverNewsPoller(object):
     def __init__(self):
 
         self.regexp_lists = {
             "overall": [re.compile("^.*확진자.*$")],
-            "suwon": [re.compile("^.*수원.*$")],
-            "gyeonggi": [re.compile("^.*경기.*$")],
+            "suwon": [re.compile("^.*수원[시]?.*$"), re.compile("^.*확진자.*$")],
+            "gyeonggi": [re.compile("^.*경기[도]?.*$"), re.compile("^.*확진자.*$")],
             "travel": [re.compile("^.*입국.*$")],
-            "samsung": [re.compile("^.*삼성.*$")],
-            "chungju": [re.compile(".*충주.*$")],
-            "india": [re.compile(".*인도.*$")]
+            "samsung": [re.compile("^.*삼성[전자]?.*$")],
+            "chungju": [re.compile(".*충주[시]?.*$")],
+            "india": [re.compile(".* 인도[는|가]? .*$")],
+            "cases": [re.compile("^.*확진자.*$"), re.compile("^.*[0-9]+명 .*$")],
+            "deaths": [re.compile(".*사망[자]?.*$")],
+            "seoul": [re.compile(".*서울[시]?.*"), re.compile("^.*확진자.*$")],
+            "paths": [re.compile(".*동선.*공개.*"), re.compile("^.*확진자.*$")],
+            "recovery": [re.compile(".*(완치|퇴원).*")]
         }
 
         self.threads = list(self.regexp_lists.keys())
@@ -38,10 +43,43 @@ class NaverNewsPoller(object):
         self.news_url = "https://openapi.naver.com/v1/search/news.json"
         self.translate_url = "https://openapi.naver.com/v1/papago/n2mt"
 
-        self.client_id = "CDIP1v_JfG9jMGOngC6i"
-        self.client_secret = "22y9ZSK1LY"
-        self.translate_client_id = "CDIP1v_JfG9jMGOngC6i"
-        self.translate_client_secret = "22y9ZSK1LY"
+        self.client_id = "AiBRs6N1cYm8xOElLPWg"
+        self.client_secret = "v0ZOrVmXIE"
+        self.translate_client_id = "AiBRs6N1cYm8xOElLPWg"
+        self.translate_client_secret = "v0ZOrVmXIE"
+
+        self.id_secret_pool = [
+            {
+                "id": "5ewcViBAJsazLgiOLgjt",
+                "secret": "ioAXZzAYU1"
+            },
+            {
+                "id": "dZyIJnbvpTlUNNH147B_",
+                "secret": "DLnIS39H61"
+            },
+            {
+                "id": "B7Rg46POPdsiycUeAbbO",
+                "secret": "KsyxHEW3Os"
+            },
+            {
+                "id": "6JESwBgS0XhGfBExl6hF",
+                "secret": "wRKOylGv3h"
+            },
+            {
+                "id": "CDIP1v_JfG9jMGOngC6i",
+                "secret": "22y9ZSK1LY"
+            },
+            {
+                "id": "AiBRs6N1cYm8xOElLPWg",
+                "secret": "v0ZOrVmXIE"
+            },
+            {
+                "id": "BEPMk7tJgBUJEzx2XGXN",
+                "secret": "oGT0d6W4oA"
+            }
+        ]
+
+        self.current_id_secret_id = 0
         self.time_polled_file = "time.txt"
 
         self.tz = pytz.timezone('Asia/Seoul')
@@ -98,16 +136,24 @@ class NaverNewsPoller(object):
                     self.new_polled_time = datetime.strptime(item["pubDate"], self.time_format)
                 self.new_polled_time = max(datetime.strptime(item["pubDate"], self.time_format), self.new_polled_time)
                 translated_string  = ""
+                parsed_uri = urlparse(item["originallink"] )
+                try:
+                    hostname = '{uri.scheme}://{uri.netloc}/'.format(uri=parsed_uri)
+                except Exception as e:
+                    hostname = "UNKNOWN"
                 for thread in self.threads:
                     if self.__check_if_include(item["title"], item["pubDate"], self.regexp_lists[thread]):
                         if translated_string == "":
                             translated_string = self.__translate(item["title"])
                         if translated_string == "":
-                            self.news_lists[thread].append(item["title"] + "\t" + item["title"] + "\t"+item["pubDate"]+"\t"+ item["link"])
+                            self.news_lists[thread].append(item["title"] + "\t" + item["title"] + "\t"+item["pubDate"]+"\t"+ item["originallink"] + "\t" + hostname)
                         else:
-                            self.news_lists[thread].append(translated_string + "\t" + item["title"] +  "\t" + item["pubDate"]+"\t"+ item["link"])
+                            self.news_lists[thread].append(translated_string + "\t" + item["title"] +  "\t" + item["pubDate"]+"\t"+ item["originallink"] + "\t" + hostname)
+            return True
         except Exception as e:
-            return
+            print(e)
+            self.current_id_secret_id = (self.current_id_secret_id + 1)%len(self.id_secret_pool)
+            return False
                 
                 
     def __add_to_file(self, filename, list_obj):
@@ -135,12 +181,13 @@ class NaverNewsPoller(object):
         try:
             return response_content["message"]["result"]["translatedText"]
         except Exception as e:
+            self.current_id_secret_id = (self.current_id_secret_id + 1)%len(self.id_secret_pool)
             return ""
     
     def __translate(self, src_str, src_lang="ko", tar_lang="en"):
         headers = {
-            "X-Naver-Client-Id": self.translate_client_id,
-            "X-Naver-Client-Secret": self.translate_client_secret,
+            "X-Naver-Client-Id": self.id_secret_pool[self.current_id_secret_id]["id"],
+            "X-Naver-Client-Secret": self.id_secret_pool[self.current_id_secret_id]["secret"],
             "Content-Type": "application/x-www-form-urlencoded"
         }
         data = {
@@ -149,12 +196,15 @@ class NaverNewsPoller(object):
             "text": src_str
         }
         resp = self.__send_request("POST", self.translate_url, headers=headers, data=data)
-        return self.__parse_translate_response(json.loads(resp.text))
+        translated_val =  self.__parse_translate_response(json.loads(resp.text))
+        if translated_val == "":
+            return self.__translate(src_str)
+        return translated_val
 
     def __search_news_and_update(self, query_str="코로나 바이러스", sort="date", start=1, display=100):
         headers = {
-            "X-Naver-Client-Id": self.client_id,
-            "X-Naver-Client-Secret": self.client_secret,
+            "X-Naver-Client-Id": self.id_secret_pool[self.current_id_secret_id]["id"],
+            "X-Naver-Client-Secret": self.id_secret_pool[self.current_id_secret_id]["secret"],
             "Content-Type": "application/json"
         }
         params = {
@@ -167,7 +217,9 @@ class NaverNewsPoller(object):
         response_text = resp.text
         response_text = response_text.replace('<b>', '')
         response_text = response_text.replace('</b>', '')
-        self.__parse_news_response(json.loads(response_text))
+        return_val = self.__parse_news_response(json.loads(response_text))
+        if(return_val == False):
+            self.__search_news_and_update()
     
     def __poll_once(self, limit=200, start=1, display=100):
         self.__load_last_polled_time()
--- webserver.py
+++ webserver.py
@@ -1,69 +1,44 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect, url_for
 
 app = Flask(__name__)
+
+NEWS_LIST_LEN = 5
 
 def read_file_to_list(filename):
     with open(filename, 'r', encoding="utf-8") as infile:
         list_news  = infile.read().splitlines()
         for i in range(0, len(list_news)):
             list_news[i] = list_news[i].split('\t')
+            while(len(list_news[i])< NEWS_LIST_LEN):
+                list_news[i].append("UNKNOWN")
         return list_news
 
-@app.route("/news/overall")
-def read_overall_news():
-    return render_template("index.html", news=read_file_to_list("overall-news.txt"))
+site_options = [["overall", "Overall"], ["suwon", "Suwon"], ["paths", "Paths"], ["deaths", "Deaths"], ["samsung", "Samsung"],  
+["travel", "Travel"], ["india", "India"], ["gyeonggi", "Gyeonggi"], ["recovery", "Recovery"], ["seoul", "Seoul"]]
+site_options_kr = [["overall", "확진자"], ["suwon", "수원"], ["paths", "동선"], ["deaths", "사망"], ["chungju", "충주"], ["seoul","서울"], ["samsung", "삼성"], 
+["gyeonggi", "경기"], ["travel", "입국"], ["india", "인도"], ["recovery", "완치"]]
 
-@app.route("/news/suwon")
-def read_suwon_news():
-    return render_template("index.html", news=read_file_to_list("suwon-news.txt"))
+@app.route("/news")
+def read_news():
+    site = request.args.get('site')
+    if site is None:
+        site = "overall"
+    return render_template("index.html", news=read_file_to_list(site + "-news.txt"), option_list=site_options, site=site)
 
-@app.route("/news/travel")
-def read_travel_news():
-    return render_template("index.html", news=read_file_to_list("travel-news.txt"))
-
-@app.route("/news/gyeonggi")
-def read_gyeonggi_news():
-    return render_template("index.html", news=read_file_to_list("gyeonggi-news.txt"))
-
-@app.route("/news/chungju")
-def read_chungju_news():
-    return render_template("index.html", news=read_file_to_list("chungju-news.txt"))
-
-@app.route("/news/samsung")
-def read_samsung_news():
-    return render_template("index.html", news=read_file_to_list("samsung-news.txt"))
-
-@app.route("/news/india")
-def read_india_news():
-    return render_template("index.html", news=read_file_to_list("india-news.txt"))
-
-@app.route("/news/overall/kr")
-def read_overall_news_kr():
-    return render_template("index-kr.html", news=read_file_to_list("overall-news.txt"))
-
-@app.route("/news/suwon/kr")
-def read_suwon_news_kr():
-    return render_template("index-kr.html", news=read_file_to_list("suwon-news.txt"))
-
-@app.route("/news/travel/kr")
-def read_travel_news_kr():
-    return render_template("index-kr.html", news=read_file_to_list("travel-news.txt"))
-
-@app.route("/news/gyeonggi/kr")
-def read_gyeonggi_news_kr():
-    return render_template("index-kr.html", news=read_file_to_list("gyeonggi-news.txt"))
-
-@app.route("/news/chungju/kr")
-def read_chungju_news_kr():
-    return render_template("index-kr.html", news=read_file_to_list("chungju-news.txt"))
-
-@app.route("/news/samsung/kr")
-def read_samsung_news_kr():
-    return render_template("index-kr.html", news=read_file_to_list("samsung-news.txt"))
-
-@app.route("/news/india/kr")
-def read_india_news_kr():
-    return render_template("index-kr.html", news=read_file_to_list("india-news.txt"))
+@app.route("/news/kr")
+def read_news_kr():
+    site = request.args.get('site')
+    if site is None:
+        site = "overall"
+    return render_template("index-kr.html", news=read_file_to_list(site + "-news.txt"), option_list=site_options_kr, site=site)
     
+# app name 
+@app.errorhandler(404) 
+# inbuilt function which takes error as parameter 
+def not_found(e): 
+  
+# defining function 
+  return redirect(url_for('read_news'))
+
 if __name__ == "__main__":
     app.run(debug=True, host='0.0.0.0')
